584d7c4a445159e4163983a3454885675c61db2a
dori medini <dorimedini@gmail.com>
2015-04-22 16:40:20 +0300

    יצירת סביבת עבודה וכמה סקריפטים
    כדי שהסקריפים יעבדו חשוב לשמור על היררכיה זהה בתוך התיקיות שלנו.
    זה גם יהיה לנו נוח להבין איפה כל דבר

---
+++ scripts/restore_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy all files. Assume the correct paths in the edited/ directory.
+cp -vr /mnt/hgfs/HW/234123-hw2/kernel_files/backup /usr/src/linux-2.4.18-14custom
---
+++ scripts/update_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Recursively dos2unix-ify all edited files before copying, so
+# no need to recurse over all files in /usr/src/linux-2.4.18-14custom/
+dos2unix /mnt/hgfs/HW/234123-hw2/kernel_files/edited/*
+
+# Copy all files. Assume the correct paths in the edited/ directory.
+cp -vr /mnt/hgfs/HW/234123-hw2/kernel_files/edited /usr/src/linux-2.4.18-14custom
