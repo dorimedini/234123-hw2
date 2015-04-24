d7dfc7fae65301693e7d816ed0c49a63f841cbda
dori medini <dorimedini@gmail.com>
2015-04-24 17:19:41 +0300

    מימוש קריאת המערכת הרביעית

--- new_files/usr/src/linux-2.4.18-14custom/kernel/hw2_syscalls.c
+++ new_files/usr/src/linux-2.4.18-14custom/kernel/hw2_syscalls.c
@@ -1,6 +1,6 @@
 
-#include <linux/sched.h>
-#include <linux/errno.h>	// Need this for the definition of EINVAL
+#include <linux/sched.h>	// Need this for definition of hw2_switch_info
+#include <linux/errno.h>	// Need this for the definition of EINVAL etc.
 
 #define NO_SUCH_PROC
 
@@ -51,7 +51,24 @@ int sys_remaining_trials(int pid) {
 
 
 
-int sys_get_scheduling_statistic(struct switch_info* info) {
+int sys_get_scheduling_statistic(hw2_switch_info* info) {
+	
+	int i, counter;
+
+	// If all indices are full, start with the oldest (hw2_logger.next_index).
+	// Otherwise, start with the oldest (zero).
+	i = hw2_logger.logged == 150? hw2_logger.next_index : 0;
+	
+	// Iterate and copy (copy exactly hw2_logger.logged entries)
+	for(counter = 0; counter < hw2_logger.logged; counter++) {
+	
+		// The copy_to_user function returns the number of bytes NOT copied,
+		// so if this value isn't zero we must return an error
+		if (copy_to_user(info+counter, &(hw2_logger.arr[i]), sizeof(hw2_logger.arr[i]))) return -EFAULT;
+	}
+	
+	// This should always be the number of copied entries
+	return hw2_logger.logged;
 	
 }
 
