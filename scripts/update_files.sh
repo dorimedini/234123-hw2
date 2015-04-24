c49328c412869f841c772f9a6807618145221fa7
dori medini <dorimedini@gmail.com>
2015-04-24 15:25:18 +0300

    הוספתי עוד קבצים שנצטרך לגעת בהם או לכתוב

--- scripts/update_files.sh
+++ scripts/update_files.sh
@@ -3,6 +3,8 @@
 # Recursively dos2unix-ify all edited files before copying, so
 # no need to recurse over all files in /usr/src/linux-2.4.18-14custom/
 dos2unix /mnt/hgfs/HW/234123-hw2/kernel_files/edited/*
+dos2unix /mnt/hgfs/HW/234123-hw2/new_files/*
 
 # Copy all files. Assume the correct paths in the edited/ directory.
 cp -vr /mnt/hgfs/HW/234123-hw2/kernel_files/edited /usr/src/linux-2.4.18-14custom
+cp -vr /mnt/hgfs/HW/234123-hw2/new_files /
