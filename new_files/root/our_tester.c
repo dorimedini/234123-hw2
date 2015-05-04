f89ecd29887f0d951e58140b4fdf1ca41726121c
ayalmit <ayalmit@gmail.com>
2015-05-04 13:13:55 +0300

    שינוי קטן

--- new_files/root/our_tester.c
+++ new_files/root/our_tester.c
@@ -2,117 +2,6 @@
  #include "test_utils.h"
  #include <stdio.h>
 
-enum SWITCH_REASONS {
-	SWITCH_UNKNOWN = 0,	// 0: Reason unknown - should cause error
-	SWITCH_CREATED,		// 1: Switched due to creation of another task
-	SWITCH_ENDED,		// 2: A task ended
-	SWITCH_YIELD,		// 3: A task gave up the CPU
-	SWITCH_OVERDUE,		// 4: A SHORT task became overdue
-	SWITCH_PREV_WAIT,	// 5: The task switched from the CPU because it started waiting
-	SWITCH_PRIO,		// 6: A task with a higher priority returned from waiting
-	SWITCH_SLICE		// 7: The previous task ran out of time
-};
-
-/**
- * Call this to create multiple processes with the
- * same policy, storing the PID of the process in pid.
- *
- * This converts the parent process to the highest
- * prioriy SHORT
- *
- * Example:
- *
- * - int pid;
- * - CREATE_PROCS(pid,10,SCHED_SHORT,4000,5);
- *
- * This should create ten SHORT processes, each with 4
- * seconds requested_time and five trials, with the
- * return value of fork() stored in pid
- */
-#define CREATE_PROCS(pid,num,policy,req,trials) do { \
-		int i; \
-		struct sched_param param = { .sched_priority = 0, .requested_time = 5000, .trial_num = 50}; \
-		sched_setscheduler(getpid(), is_SHORT(getpid()) ? -1 : SCHED_SHORT, &param); \
-		nice(-19); \
-		for (i=0; i<num; ++i) { \
-			pid = fork(); \
-			if (!pid) { \
-				param = { .sched_priority = 0, .requested_time = req, .trial_num = trials}; \
-				sched_setscheduler(getpid(), policy, &param); \
-				break; \
-			} \
-		} \
-	} while(0)
-
-/**
- * To destroy all processes created via CREATE_PROCS,
- * call this with the pid variable sent to CREATE_PROCS.
- * Only the original parent will execute the code after
- * this macro.
- *
- * Example:
- *
- * - ... code executed by one process...
- * - int pid;
- * - CREATE_PROCS(pid,10,SCHED_SHORT,4000,5);
- * - ... more code, 11 processes executing it...
- * - if (pid) CREATE_PROCS(pid,5,SCHED_OTHER, 0,0);
- * - ... more code, 16 processes executing it...
- * - EXIT_PROCS(pid);
- * - ... more code, only one process (the original process) executing it...
- *
-*/
-#define EXIT_PROCS(pid) do { \
-		if (!pid) exit(0); \
-		else while(wait() != -1); \
-	} while(0)
-
-#define CREATE_SHORT(pid)
-do { \
-		struct sched_param param = { .sched_priority = 0, .requested_time = 2000, .trial_num = 20}; \
-		sched_setscheduler(pid, is_SHORT(pid) ? -1 : SCHED_SHORT, &param); \
-	} while(0)
-
-#define BEST_SHORT(pid) do { \
-		struct sched_param param = { .sched_priority = 0, .requested_time = 5000, .trial_num = 50}; \
-		sched_setscheduler(pid, is_SHORT(pid) ? -1 : SCHED_SHORT, &param); \
-		nice(-19); \
-	} while(0)
-
-/** Self explanatory */
-int calculate_fibo(int num) {
-	if (num < 2) return num;
-	return calculate_fibo(num-1)+calculate_fibo(num-2);
-}
-
-int getRelevantLogger(int* relevantPids, int n ,
-						struct switch_info* relevantLogger, bool print) {
-	int index = 0;
-	struct switch_info info[150];
-	// Get the statistics
-	int num_switches = get_scheduling_statistic(info);
-	for (int i = 0 ; i < num_switches ; ++i) {
-		if (findTheRelevant(relevantPids,n,info[i].previous_pid)) {
-			relevantLogger[index] = info[i];
-			++index;
-		}
-	}
-	if (print) {
-		print_info(info, num_switches);
-		printf ("\n\n The Relevant Logger:\n");
-		print_info(relevantLogger, index);
-	}
-	return index;
-}
-
-int findTheRelevant(int* relevantPids, int n,int thePidToFind) {
-	for (int i = 0 ; i < n ; ++i) {
-		if (relevantPids[i] == thePidToFind) {
-			return 1;
-		}
-	}
-	return 0;
-}
 
 /*
  * TEST: An OTHER Proc created a new proc which became short.  The proiority of the new proc (The short one)
@@ -121,31 +10,40 @@ int findTheRelevant(int* relevantPids, int n,int thePidToFind) {
  * RESULT: The new proc should run and after it finish the old proc
  * should run
 */
-bool other_to_short_with_high_prio() {
+/*
+ * TEST: An OTHER Proc created a new proc which became short.  The proiority of the new proc (The short one)
+ * is lower than the old one (the OTHER) .
+ *
+ * RESULT: The new proc should run and after it finish the old proc
+ * should run
+*/
+bool other_to_short_with_low_prio() {
 	int relevantPids[2];
 	int dad_pid = getpid();
-	int son_pid;
-	relevantPids[0] = dad_pid;
-	int pid = fork(); // The major dad.
-	if (!pid) {
-		son_pid = getpid();
-		relevantPids[1] = son_pid;
-		BEST_SHORT(pid); // make the son short
-		calculate_fibo(15);
+	int son_pid = pid;
+	REMEMBER_RELEVANT(dad_pid, relevantPids, 0)
+	CREATE_PROC(pid, SCHED_SHORT, 2000, 10, 2);
+	if (!pid) {    									// son proc. the tested proc
+		while (remaining_trials(son_pid) > 1) {}	// it will stop running just before it becomes overdue
+		exit(0);
 	} else {
-		calculate_fibo(30);
+		son_pid = pid;
+		REMEMBER_RELEVANT(son_pid, relevantPids, 1);
+		calculate_fibo(40);
+		EXIT_PROCS(dad_pid);
+		
+		struct switch_info info[150];
+		int size =  getRelevantLogger(relevantPids,2,info, 0);
+
+		TEST_EQUALS(size, 3);
+		TEST_EQUALS(info[0].previous_pid, dad_pid);
+		TEST_EQUALS(info[1].previous_pid, son_pid);
+		TEST_EQUALS(info[1].reason, SWITCH_ENDED);
+		TEST_EQUALS(info[2].previous_pid, dad_pid);
 	}
-	struct switch_info info[150];
-	int size =  getRelevantLogger(relevantPids,2,info, 0);
-
-	TEST_EQUALS(size, 3);
-	TEST_EQUALS(info[0].previous_pid, dad_pid);
-	TEST_EQUALS(info[1].previous_pid, son_pid);
-	TEST_EQUALS(info[1].reason, SWITCH_ENDED);
-	TEST_EQUALS(info[2].previous_pid, dad_pid);
-
 return true;
 }
+
 
 /*
  * TEST: An OTHER Proc created a new proc which became short.  The proiority of the new proc (The short one)
@@ -157,33 +55,72 @@ return true;
 bool other_to_short_with_low_prio() {
 	int relevantPids[2];
 	int dad_pid = getpid();
-	int son_pid;
-	relevantPids[0] = dad_pid;
-	int pid = fork(); // The major dad.
-	if (!pid) {
-		son_pid = getpid();
-		relevantPids[1] = son_pid;
-		CREATE_SHORT(pid) // make the son short
-		nice(5);
-		calculate_fibo(15);
+	int son_pid = pid;
+	REMEMBER_RELEVANT(dad_pid, relevantPids, 0)
+	CREATE_PROC(pid, SCHED_SHORT, 2000, 10, -2);
+	if (!pid) {    									// son proc. the tested proc
+		while (remaining_trials(son_pid) > 1) {}	// it will stop running just before it becomes overdue
+		exit(0);
 	} else {
-		calculate_fibo(30);
+		son_pid = pid;
+		REMEMBER_RELEVANT(son_pid, relevantPids, 1);
+		calculate_fibo(40);
+		EXIT_PROCS(dad_pid);
+		
+		struct switch_info info[150];
+		int size =  getRelevantLogger(relevantPids,2,info, 0);
+
+		TEST_EQUALS(size, 3);
+		TEST_EQUALS(info[0].previous_pid, dad_pid);
+		TEST_EQUALS(info[1].previous_pid, son_pid);
+		TEST_EQUALS(info[1].reason, SWITCH_ENDED);
+		TEST_EQUALS(info[2].previous_pid, dad_pid);
 	}
-	struct switch_info info[150];
-	int size =  getRelevantLogger(relevantPids,2,info, 0);
+return true;
+}
 
-	TEST_EQUALS(size, 3);
-	TEST_EQUALS(info[0].previous_pid, dad_pid);
-	TEST_EQUALS(info[1].previous_pid, son_pid);
-	TEST_EQUALS(info[1].reason, SWITCH_ENDED);
-	TEST_EQUALS(info[2].previous_pid, dad_pid);
 
+/*
+ * TEST: An OTHER Proc created a new SHORT with only few trials. 
+ *
+ * RESULT: The new proc should run but after a while it should become overdue, and then
+ * the old proc should run
+*/
+bool short_who_finishes_his_trials() {
+	int relevantPids[2];
+	int dad_pid = getpid();
+	int son_pid = pid;
+	REMEMBER_RELEVANT(dad_pid, relevantPids, 0)
+	CREATE_PROC(pid, SCHED_SHORT, 2000, 3, -2);			// Create a Short with few trials
+	if (!pid) {    									// son proc. the tested proc
+		calculate_fibo(50);							// give him some shitty task. should become overdue							
+		exit(0);
+	} else {
+		son_pid = pid;
+		REMEMBER_RELEVANT(son_pid, relevantPids, 1);
+		calculate_fibo(40);
+		EXIT_PROCS(dad_pid);
+		
+		struct switch_info info[150];
+		int size =  getRelevantLogger(relevantPids,2,info, 0);
+
+		TEST_EQUALS(info[0].previous_pid, dad_pid);
+		TEST_EQUALS(info[1].previous_pid, son_pid);
+		TEST_EQUALS(info[1].reason, SWITCH_OVERDUE);
+		TEST_EQUALS(info[2].previous_pid, dad_pid);
+	}
 return true;
 }
 
 
 int main() {
-	other_to_short_with_high_prio();
-	other_to_short_with_low_prio();
+	RUN_TEST(other_to_short_with_high_prio());
+	RUN_TEST(other_to_short_with_low_prio());
+	RUN_TEST(short_who_finishes_his_trials());
+
 	return 0;
 }
+
+
+
+
