03da70c14fc9738af8d8a4616e4cfc73a6ee5469
ayalmit <ayalmit@gmail.com>
2015-05-05 20:06:22 +0300

    .עוד קמפול
    עובר לידיו המסורות של שמעון להמשך מלחמה בקמפול

--- new_files/root/our_tester.c
+++ new_files/root/our_tester.c
@@ -261,7 +261,7 @@ bool testShortFork()
 		EXIT_PROCS(getPid());
 
 	} else {
-		while(!is_short(getPid())) {}	// wait until your dad set you to SHORT
+		while(!is_short(getPid())) {;}	// wait until your dad set you to SHORT
 
 		TEST_EQUALS(remaining_time(getpid()), expected_requested_time);
 		expected_trials = remaining_trials(getPid());
@@ -271,7 +271,7 @@ bool testShortFork()
 			int grandson_initial_time = remaining_time(getpid());
 			TEST_TRUE(grandson_initial_time <= expected_requested_time/2);
 			TEST_TRUE(grandson_initial_time > 0);
-			TEST_TRUE(is_short(getPid());
+			TEST_TRUE(is_short(getPid()));
 			TEST_EQUALS(remaining_trials(getPid()), expected_trials/2);
 			doMediumTask();
 			TEST_TRUE(remaining_time(getpid()) < grandson_initial_time);
@@ -306,9 +306,10 @@ bool testOverdueFork()
 		int son = fork();
 		if (son == 0)
 		{
-			TEST_EQUALS(remaining_trials(getPid()));
+			TEST_FALSE(remaining_trials(getPid()));
 			doShortTask();
-			TEST_TRUE(remaining_time(getpid()) < grandson_initial_time);
+			TEST_FALSE(remaining_time(getPid()));
+			TEST_FALSE(is_short(getPid()));
 			_exit(0);
 		}
 		else
@@ -342,7 +343,7 @@ void testScheduleRealTimeOverShort()
 		{
 			struct sched_param param;
 			param.sched_priority = 1;
-			CHANGE_TO_SHORT(id,expected_requested_time,expected_trials)	// SHORT process
+			CHANGE_TO_SHORT(id,expected_requested_time,expected_trials);	// SHORT process
 			sched_setscheduler(id2, SCHED_FIFO, &param);				//FIFO RealTime process
 		}
 		EXIT_PROCS(getPid());
@@ -400,7 +401,7 @@ void testScheduleShortOverOther()
 		int id2 = fork();
 		if (id2 == 0)
 		{
-			while (!is_short(getPid)) {}
+			while (!is_short(getPid())) {}
 			doMediumTask();
 			printf("\tSHORT son finished\n");
 			_exit(0);
@@ -514,7 +515,7 @@ bool testScheduleOtherShortWhoBecameOverdue()
 		int id2 = fork();
 		if (id2 == 0)
 		{
-			while (!is_short(getPid)) {}
+			while (!is_short(getPid())) {}
 			printf("\tSHORT son started\n");
 			while (remaining_trials(getPid()) > 0) {
 				if (remaining_trials(getPid()) >1) {
@@ -540,15 +541,15 @@ bool testScheduleOtherShortWhoBecameOverdue()
 }
 
 int main() {
-	RUN_TEST(testTaskEnded());
-	RUN_TEST(testTaskYield());
-	RUN_TEST(testTaskBecomeOverDue());
-	RUN_TEST(testTaskPreviousWait());
-	RUN_TEST(testMakeSonShort());
-	RUN_TEST(testBadParams());
-	RUN_TEST(testSysCalls());
-	RUN_TEST(testShortFork());
-	RUN_TEST(testOverdueFork());
+	RUN_TEST(testTaskEnded);
+	RUN_TEST(testTaskYield);
+	RUN_TEST(testTaskBecomeOverDue);
+	RUN_TEST(testTaskPreviousWait);
+	RUN_TEST(testMakeSonShort);
+	RUN_TEST(testBadParams);
+	RUN_TEST(testSysCalls);
+	RUN_TEST(testShortFork);
+	RUN_TEST(testOverdueFork);
 
 
 	// Those tests focus on the competition between to different policies.
@@ -573,7 +574,7 @@ int main() {
 
 	printf("Testing race: OTHER vs. SHORT who became OVERDUE\n");
 	printf("SHORT is supposed to begin, then to become overdue, OTHER supposed to finish\n");
-	printf("and then OVERDUE finish:");
+	printf("and then OVERDUE finish:\n");
 	testScheduleOtherShortWhoBecameOverdue();
 
 
@@ -586,257 +587,3 @@ int main() {
 	*/
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Our OLD TESTS. MAYBE WE CAN USE THEM LATER ON
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// WHEN OTHER IS RUNNING...
-
-
-// Other proc will create a son process that will become short process
-
-
-
-
-
-bool other_to_short_with_high_prio() {
-	int relevantPids[2];
-	int otherPid = getpid();
-	int shortPid;
-
-	// Rememeber the other pid
-	relevantPids[0] = otherPid
-	shortPid = fork();
-	if (!shortPid) {
-		// We are inside the son , let's make him short with high prio.
-		set_to_SHORT_with_prio(getpid(),20,20,-19);
-		// This short process should now run.
-		while (remaining_trials(getpid()) > 1) {}	// it will stop running just before it becomes overdue
-		// Kill this short
-		exit(0);
-	} else { // Inside the dad
-		relevantPids[1] = shortPid;		
-		EXIT_PROCS(1);
-		//Give the process we want to test long job
-		calculate_fibo(30);
-		// switch info array for our relevent switches
-		struct switch_info info[150];
-		int size =  getRelevantLogger(relevantPids,2,info, 0);
-		// Check the switches
-		return checkTheReleventSwitches(otherPid,shortPid,info,size);	
-	}
-	return false;
-}
-
-
-
-
-// Other proc will create a son process that will become short process 
-bool other_to_short_with_low_prio() {
-	int relevantPids[2];
-	int otherPid = getpid();
-	int shortPid;
-
-
-	// Rememeber the other pid
-	relevantPids[0] = otherPid
-	shortPid = fork();
-	if (!shortPid) {
-		// We are inside the son , let's make him short with high prio.
-		set_to_SHORT_with_prio(getpid(),20,20,0);
-		// This short process should now run.
-		while (remaining_trials(getpid()) > 1) {}	// it will stop running just before it becomes overdue
-		// Kill this short
-		exit(0);
-	} else { // Inside the dad
-		//Give the process we want to test long job
-		calculate_fibo(30);
-		relevantPids[1] = shortPid;		
-		// switch info array for our relevent switches
-		EXIT_PROCS(1);
-		struct switch_info info[150];
-		int size =  getRelevantLogger(relevantPids,2,info, 0);
-		// Check the switches
-		return checkTheReleventSwitches(otherPid,shortPid,info,size);	
-	}
-	return false;
-}
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// WHEN SHORT IS RUNNING...
-
-// Section 1
-bool rt_is_sched() {
-	int relevantPids[2];
-
-
-	// Creating two sons
-	for (int i = 0 ; i < 2 ; ++i) {
-		int pid = fork();
-		if (!pid) {
-			// Give each son long job
-			calculate_fibo(30);
-			break;
-		} else {
-			// Insert the sons in to the array
-			relevantPids[i] = pid;
-		}
-	}
-	// Make the first son to be short and the other real time 
-
-	// Ayal continue..
-	
-
-}
-
-
-// Section 2
-bool short_is_sched_high_prio() {
-	
-}
-
-
-// Section 3
-bool short_is_sched_same_prio() {
-	
-}
-
-// Section 4
-bool other_is_sched_high_prio() {
-	
-}
-
-// Section 5
-bool other_is_sched_low_prio() {
-	
-}
-
-// Section 6
-// No shorts left in the runnig short curr prio and there is low prio short to run 
-bool low_prio_short() {
-	
-}
-
-// Section 7
-// The last short is the running short and there are shorts in the same prio and short in lowest prio
-bool other_shorts_in_the_same_prio() {
-	
-}
-
-
-
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
-
-*/
-
-/*
- * TEST: We use a SHORT proc with very high prio as a master proc. It create a first SHORT proc with
- * than create another SHORT proc with same prio. Both procs has 3 trials. they should run till they have
- * 1 trial left.
- *
- * RESULT: It should look like that: dad->son1->son2->son1->son2->dad
-*
-bool two_shorts_different_prio() {
-	int pid1;
-	CREATE_PROC(pid, SCHED_SHORT, 5000, 50, -19);	//create a master proc to control the test
-	if (!pid1) {    									
-		int relevantPids[3];
-		int dad_pid = getpid();
-		REMEMBER_RELEVANT(dad_pid, relevantPids, 0);	
-		int pid2;
-		int pid3
-		CREATE_PROC(pid2, SCHED_SHORT, 2000, 3, 0);	// create a new short with static prio of +2
-		if (!pid2) {
-			while (remaining_trials(son_pid) > 1) {}	// it will stop running just before it becomes overdue
-			exit(0);
-		} else {
-			int son_pid1 = pid2;
-			REMEMBER_RELEVANT(son_pid1, relevantPids, 1);	// remember the first son
-			CREATE_PROC(pid3, SCHED_SHORT, 2000, 3, 0);	// create another shor with static prio of +1
-			if (!pid3) {
-				while (remaining_trials(son_pid) > 1) {}	// it will stop running just before it becomes overdue
-				exit(0);
-			} else {
-				int son_pid2 = pid3;
-				REMEMBER_RELEVANT(son_pid2, relevantPids, 2);	// remember the second son
-				EXIT_PROCS(dad_pid);
-
-				struct switch_info info[150];
-				int size =  getRelevantLogger(relevantPids,2,info, 0);
-
-				TEST_EQUALS(size, 6);
-				TEST_EQUALS(info[0].previous_pid, dad_pid);
-				TEST_EQUALS(info[1].previous_pid, son_pid1);
-				TEST_EQUALS(info[1].reason, SWITCH_SLICE);
-				TEST_EQUALS(info[2].previous_pid, son_pid2);
-				TEST_EQUALS(info[2].reason, SWITCH_SLICE);
-				TEST_EQUALS(info[3].previous_pid, son_pid1);
-				TEST_EQUALS(info[3].reason, SWITCH_ENDED);
-				TEST_EQUALS(info[4].previous_pid, son_pid2);
-				TEST_EQUALS(info[4].reason, SWITCH_ENDED);
-				TEST_EQUALS(info[5].previous_pid, dad_pid);
-			}
-		}
-	}
-	EXIT_PROCS(getpid());
-	return true;
-}
-
-
-/*
- * TEST: An OTHER Proc created a new SHORT with only few trials. 
- *
- * RESULT: The new proc should run but after a while it should become overdue, and then
- * the old proc should run
-*
-bool short_who_finishes_his_trials() {
-	int relevantPids[2];
-	int dad_pid = getpid();
-	int pid;
-	REMEMBER_RELEVANT(dad_pid, relevantPids, 0);
-	CREATE_PROC(pid, SCHED_SHORT, 2000, 3, -2);			// Create a Short with few trials
-	if (!pid) {    									// son proc. the tested proc
-		calculate_fibo(50);							// give him some shitty task. should become overdue							
-		exit(0);
-	} else {
-		int son_pid = pid;
-		REMEMBER_RELEVANT(son_pid, relevantPids, 1);
-		calculate_fibo(40);
-		EXIT_PROCS(dad_pid);
-		
-		struct switch_info info[150];
-		int size =  getRelevantLogger(relevantPids,2,info, 0);
-
-		TEST_EQUALS(info[0].previous_pid, dad_pid);
-		TEST_EQUALS(info[1].previous_pid, son_pid);
-		TEST_EQUALS(info[1].reason, SWITCH_OVERDUE);
-		TEST_EQUALS(info[2].previous_pid, dad_pid);
-	}
-return true;
-}
-*/
