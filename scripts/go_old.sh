8560ecc71ee73c52b7a9421a63e594eb50d5ef0b
dori medini <dorimedini@gmail.com>
2015-06-14 15:12:45 +0300

    הוספת סקריפטים לשמעון

---
+++ scripts/go_old.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update all files
+cd /mnt/hgfs/HW/234123-hw2/scripts/
+./update_files_old.sh
+
+# Now, build the kernel
+cd /usr/src/linux-2.4.18-14custom
+make bzImage
+cd arch/i386/boot
+cp -f bzImage /boot/vmlinuz-2.4.18-14custom
---
+++ scripts/update_files_old.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Go to the dir
+cd /root/
+mkdir -p hw2
+cd /root/hw2/
+mkdir -p tests
+
+# Recursively dos2unix-ify all edited files before copying, so
+# no need to recurse over all files in /usr/src/linux-2.4.18-14custom/
+dos2unix /mnt/hgfs/HW/234123-hw2/submission_11.5/kernel/*
+dos2unix /mnt/hgfs/HW/234123-hw2/new_files/*
+
+# Copy all files. Assume the correct paths in the edited/ directory.
+cp -vr /mnt/hgfs/HW/234123-hw2/submission_11.5/kernel/* /usr/src/linux-2.4.18-14custom/
+cp -vr /mnt/hgfs/HW/234123-hw2/new_files/* ./
+cp -vr /mnt/hgfs/HW/234123-hw2/appeal_test/* ./tests/
